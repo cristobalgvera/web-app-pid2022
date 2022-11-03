1b894f142c568234acdc25c067ffd1af85157335
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-03 19:51:41 -0300

    chore(dev): add husky and lint-staged workflow

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+
+module.exports = {
+  "*": "npm run format --",
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
