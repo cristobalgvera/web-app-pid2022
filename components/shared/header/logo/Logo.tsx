97957580b8285203e0a1313f2936fc824f2678fb
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-04 04:52:10 -0300

    feat(logo): add link to homepage

--- components/shared/header/logo/Logo.tsx
+++ components/shared/header/logo/Logo.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export const Logo = () => {
   return (
-    <div className="hover:cursor-pointer">
+    <Link href="/">
       <h1 className="text-6xl font-bold md:text-5xl xl:text-4xl">Calculadora</h1>
-    </div>
+    </Link>
   );
 };
