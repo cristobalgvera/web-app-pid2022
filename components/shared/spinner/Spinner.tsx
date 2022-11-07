e56aaee55c6a83d4688b01bc68fd2d777de1a6fa
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-06 22:39:38 -0300

    feat(spinner): create common circle spinner

---
+++ components/shared/spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-primary-400"></div>
+    </div>
+  );
+};
