7ba63574673e6af781e6eb43938e4ba3f1e70cc3
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-10 00:37:31 -0300

    refactor(api): create lib/api folder to store requests

--- app/(pages)/tutorials/[id]/page.tsx
+++ app/(pages)/tutorials/[id]/page.tsx
@@ -1,34 +1,9 @@
-import { Tutorial, TutorialModel } from "@components/tutorials";
-import { requestSdk } from "@graphql/util";
 import { NextPageProps } from "@utils/types";
-import { notFound } from "next/navigation";
-import { serialize } from "next-mdx-remote/serialize";
-
-async function getTutorial(id: string): Promise<TutorialModel> {
-  const response = await requestSdk.getOneTutorial({ id });
-
-  if (!response.tutorial?.data?.id) notFound();
-
-  const html = await serialize(response.tutorial.data.attributes?.content ?? "");
-
-  return {
-    id: response.tutorial.data.id ?? "",
-    title: response.tutorial.data.attributes?.title ?? "",
-    categories:
-      response.tutorial.data.attributes?.categories?.data
-        .map((category) => category.attributes?.name ?? "")
-        .filter(Boolean) ?? [],
-    content: html,
-    summary: response.tutorial.data.attributes?.summary ?? "",
-    cover: {
-      src: response.tutorial.data.attributes?.cover?.data?.attributes?.url ?? "",
-      alt: response.tutorial.data.attributes?.cover?.data?.attributes?.alternativeText ?? "",
-    },
-  };
-}
+import { tutorialApi } from "@lib/api";
+import { Tutorial } from "@components/tutorials";
 
 export default async function TutorialPage({ params }: NextPageProps<{ id: string }>) {
-  const tutorial = await getTutorial(params.id);
+  const tutorial = await tutorialApi.getTutorial(params.id);
 
   return <Tutorial tutorial={tutorial} />;
 }
---
+++ lib/api/tutorial.ts
@@ -0,0 +1,31 @@
+import { TutorialModel } from "@components/tutorials";
+import { requestSdk } from "@graphql/util";
+import { serialize } from "next-mdx-remote/serialize";
+import { notFound } from "next/navigation";
+
+async function getTutorial(id: string): Promise<TutorialModel> {
+  const response = await requestSdk.getOneTutorial({ id });
+
+  if (!response.tutorial?.data?.id) notFound();
+
+  const html = await serialize(response.tutorial.data.attributes?.content ?? "");
+
+  return {
+    id: response.tutorial.data.id ?? "",
+    title: response.tutorial.data.attributes?.title ?? "",
+    categories:
+      response.tutorial.data.attributes?.categories?.data
+        .map((category) => category.attributes?.name ?? "")
+        .filter(Boolean) ?? [],
+    content: html,
+    summary: response.tutorial.data.attributes?.summary ?? "",
+    cover: {
+      src: response.tutorial.data.attributes?.cover?.data?.attributes?.url ?? "",
+      alt: response.tutorial.data.attributes?.cover?.data?.attributes?.alternativeText ?? "",
+    },
+  };
+}
+
+export const tutorialApi = {
+  getTutorial,
+};
