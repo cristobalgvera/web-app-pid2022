7fb305c1c5ee8070cd1d3043b405a57dae4a4d0b
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-16 02:09:15 -0300

    feat(spinner): increase heigth to spinner container

--- app/components/shared/spinner/Spinner.tsx
+++ app/components/shared/spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 export const Spinner = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex h-64 items-center justify-center">
       <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-primary-400"></div>
     </div>
   );
