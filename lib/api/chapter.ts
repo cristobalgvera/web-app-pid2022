88fece8827f68f622c4d7291572199296b66927a
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-13 03:19:12 -0300

    feat(chapter): add get by chapter id function

--- lib/api/chapter.ts
+++ lib/api/chapter.ts
@@ -1,5 +1,8 @@
-import { ChapterIndexModel } from "@components/chapters";
+import { ChapterEntryModel, ChapterIndexModel } from "@components/chapters";
+import { GetChapterByIdQueryVariables } from "@graphql/generated/graphql";
 import { requestSdk } from "@graphql/util";
+import { parseHtml } from "@utils/content";
+import { notFound } from "next/navigation";
 
 async function getTutorialIndexes(): Promise<ChapterIndexModel[]> {
   const response = await requestSdk.getTutorialIndexes();
@@ -22,6 +25,36 @@ async function getTutorialIndexes(): Promise<ChapterIndexModel[]> {
   }));
 }
 
+async function getChapterById(variables: GetChapterByIdQueryVariables): Promise<ChapterEntryModel> {
+  const response = await requestSdk.getChapterById(variables);
+
+  const chapter = response.chapter?.data;
+
+  if (!chapter) return notFound();
+
+  const { title, content, topics } = chapter.attributes ?? {};
+
+  const html = parseHtml(content);
+
+  return {
+    title: title ?? "",
+    content: html,
+    topics:
+      topics?.data.map((topic) => ({
+        id: topic.id ?? "",
+        title: topic.attributes?.title ?? "",
+        summary: topic.attributes?.summary ?? "",
+        cover: topic.attributes?.cover?.data?.attributes?.url
+          ? {
+              src: topic.attributes.cover.data.attributes.url,
+              alt: topic.attributes.cover.data.attributes.alternativeText ?? "",
+            }
+          : undefined,
+      })) ?? [],
+  };
+}
+
 export const chapterApi = {
+  getChapterById,
   getTutorialIndexes,
 };
