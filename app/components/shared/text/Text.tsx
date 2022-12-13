14eaf26f2c658cbf5f1d9b4f0879b9b7edd92fe7
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-13 01:16:24 -0300

    refactor(text): add better ui for titles and subtitles

--- app/components/shared/text/Text.tsx
+++ app/components/shared/text/Text.tsx
@@ -6,9 +6,9 @@ interface TitleProps extends TextProps {}
 interface SubtitleProps extends TextProps {}
 
 export const Title = ({ children }: TitleProps) => {
-  return <h1 className="mb-6 text-center text-6xl font-bold">{children}</h1>;
+  return <h1 className="mb-6 text-center text-xl font-bold sm:text-2xl md:text-3xl">{children}</h1>;
 };
 
 export const Subtitle = ({ children }: SubtitleProps) => {
-  return <h2 className="mb-4 text-center text-4xl font-bold">{children}</h2>;
+  return <h2 className="mb-4 text-center text-lg font-bold sm:text-xl md:text-2xl">{children}</h2>;
 };
