3249590602ab60f3f9ed0beebfb4f51d5a58d849
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-21 20:04:47 -0300

    feat(button): add class name capabilities to go back button

--- app/components/shared/button/GoBackButton.tsx
+++ app/components/shared/button/GoBackButton.tsx
@@ -1,16 +1,18 @@
-"use client";
-
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 import { Button } from "./Button";
 
-export const GoBackButton = () => {
+interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
+
+export const GoBackButton = ({ className, ...props }: ButtonProps) => {
   const router = useRouter();
 
   return (
     <Button
       onClick={router.back}
-      className="absolute rounded-full bg-brand-secondary hover:bg-secondary-700"
+      className={`rounded-full bg-brand-secondary hover:bg-secondary-700 ${className}`}
+      {...props}
     >
       <ChevronLeftIcon className="w-4" />
     </Button>
