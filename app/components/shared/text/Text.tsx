c25fb5e2cbebf58b7b6dac246820faef7088f670
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-21 19:19:34 -0300

    refactor(text): apply same text-size to all screen sizes

--- app/components/shared/text/Text.tsx
+++ app/components/shared/text/Text.tsx
@@ -6,9 +6,9 @@ interface TitleProps extends TextProps {}
 interface SubtitleProps extends TextProps {}
 
 export const Title = ({ children }: TitleProps) => {
-  return <h1 className="mb-1 text-2xl font-bold sm:text-center md:text-3xl">{children}</h1>;
+  return <h1 className="mb-1 text-3xl font-bold sm:text-center">{children}</h1>;
 };
 
 export const Subtitle = ({ children }: SubtitleProps) => {
-  return <h2 className="text-xl font-bold sm:text-center md:text-2xl">{children}</h2>;
+  return <h2 className="text-2xl font-bold sm:text-center">{children}</h2>;
 };
